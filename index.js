1098b60be8e00a8eea8b5c7863d63db8dfc50fd1
unknown <gabriel.cesar.pereira.9@gmail.com>
2024-04-09 22:36:21 -0300

    add appuse11

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin:'https://flyenglish-orcin.vercel.app',
     methods:['GET','PUT','POST','DELETE','OPTIONS'],
     credentials:true,
-    allowedHeaders: ['Content-Type']
+    allowedHeaders:['Accept', 'Accept-Language', 'Content-Language', 'Content-Type']
 }))
 
 app.get('/check',jsonParser, (req, res)=>{
